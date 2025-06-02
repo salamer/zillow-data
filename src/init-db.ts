a8e81758738acbfe3f7ed55c4538f02699f11448
salamer <salameryy@163.com>
2025-06-03 01:43:35 +0800

    add: add demo auth

--- src/init-db.ts
+++ src/init-db.ts
@@ -1,11 +1,15 @@
 // This script initializes the database by creating the schema and synchronizing the tables
 
-import config from './config';
-import { AppDataSource, schema, House, User } from './models';
-import { hashPassword } from './utils';
+import config from "./config";
+import { AppDataSource, schema, House, User } from "./models";
+import { hashPassword } from "./utils";
+import initdata from "./zillow-init-data/data.json";
+import fs from "fs";
+import { uploadBase64ToObjectStorage } from "./objectstorage.service";
+import path from "path";
 
 export async function initializeDatabase() {
-  console.log('Initializing database...');
+  console.log("Initializing database...");
   // connect to the database
   await AppDataSource.initialize();
 
@@ -15,7 +19,7 @@ export async function initializeDatabase() {
 
   // synchronize the database schema
   // This will create the tables if they don't exist
-  console.log('Synchronizing database schema...');
+  console.log("Synchronizing database schema...");
   await AppDataSource.synchronize();
 
   // create gin index for houses table
@@ -29,27 +33,53 @@ export async function initializeDatabase() {
   const repo = AppDataSource.getRepository(User);
   const adminUser = repo.create({
     username: config.ADMIN_USERNAME,
-    email: 'admin@admin.org',
-    passwordHash: await hashPassword('admin123'),
+    email: "admin@admin.org",
+    passwordHash: await hashPassword("admin123"),
     id: config.ADMIN_USER_ID, // Set a fixed ID for the admin user
   });
   await repo.save(adminUser);
 
   const guestUser = repo.create({
     username: config.GUEST_USERNAME,
-    email: 'guest@guest.org',
-    passwordHash: await hashPassword('guest123'),
+    email: "guest@guest.org",
+    passwordHash: await hashPassword("guest123"),
     id: config.GUEST_USER_ID, // Set a fixed ID for the guest user
   });
   await repo.save(guestUser);
+
+  // init data
+  for (var i = 0; i < initdata.length; i++) {
+    const houseData = initdata[i];
+    const image = fs.readFileSync(
+      path.join(__dirname, "zillow-init-data", `${i}.jpg`)
+    );
+    const imageBase64 = image.toString("base64");
+    const uploadResult = await uploadBase64ToObjectStorage(
+      imageBase64,
+      "image/jpeg"
+    );
+    const house = AppDataSource.getRepository(House).create({
+      userId: config.ADMIN_USER_ID, // Use the admin user for initial data
+      imageUrl: uploadResult.objectUrl,
+      caption: houseData.caption || null,
+      price: houseData.price,
+      address: houseData.address,
+      state: houseData.state,
+      city: houseData.city,
+      zipCode: houseData.zipCode,
+      size: houseData.size,
+    });
+    await AppDataSource.getRepository(House).save(house);
+    console.log(`House ${i + 1} initialized: ${houseData.caption}`);
+  }
 }
 // This function will be called when the script is run
 initializeDatabase()
   .then(() => {
-    console.log('Database initialized successfully');
+    console.log("Database initialized successfully");
     process.exit(0);
   })
   .catch((error) => {
-    console.error('Error initializing database:', error);
+    console.error("Error initializing database:", error);
     process.exit(1);
   });
